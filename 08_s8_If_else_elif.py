30d229b7337040da27cc328cfdef5b66881956c3
samishah11 <116480008+samishah11@users.noreply.github.com>
2022-11-07 23:56:51 -0800

    my first comment

---
+++ 08_s8_If_else_elif.py
@@ -0,0 +1,33 @@
+saad_age = 4
+required_age_at_school = 5
+# question: can saad go to school
+
+if saad_age==required_age_at_school:
+   print("saad_can_join_the_school")
+else:
+   print("saad can not go to school")
+
+
+
+   # About me
+   my_age = 21
+   required_age_at_collage = 19
+   # can i join the collage ?
+   if my_age==required_age_at_collage:
+      print("I am a student")
+   elif my_age>required_age_at_collage:
+      print("I go to university")
+   else:
+      print("I am a former")
+
+
+
+
+ali_age = 14
+required_age_at_collage = 15
+#   can ali jion the school 
+if ali_age==required_age_at_collage:
+   print("he go to school")
+else:
+   print("he is a loser")
+
